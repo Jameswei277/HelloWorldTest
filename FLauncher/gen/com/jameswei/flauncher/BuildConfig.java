d2b0c45820855a6c65885cb7be1c41e6aaeabbfa
Jameswei <gongzhw@fiberhome.com.cn>
2015-02-03 16:11:10 +0800

    Add start code launcher!!
    Signed-off-by: Jameswei <gongzhw@fiberhome.com.cn>

---
+++ FLauncher/gen/com/jameswei/flauncher/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.jameswei.flauncher;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ FLauncher/src/com/jameswei/flauncher/LauncherActivity.java
@@ -0,0 +1,16 @@
+package com.jameswei.flauncher;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class LauncherActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.launcher_layout);
+
+	}
+
+}
